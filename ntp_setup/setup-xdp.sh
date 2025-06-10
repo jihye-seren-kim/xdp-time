89bbc4a9f91fffe45a219442402152cc1ea82df3
Jihye <90093253+jihye-seren-kim@users.noreply.github.com>
2025-06-10 14:45:05 +0200

    temporary

--- ntp_setup/setup-xdp.sh
+++ ntp_setup/setup-xdp.sh
@@ -4,10 +4,13 @@ set -e
 echo "[+] Installing dependencies..."
 sudo apt update
 sudo apt install -y clang llvm ethtool iproute2 bpfcc-tools libelf-dev gcc-multilib build-essential
+sudo apt install linux-headers-$(uname -r) linux-image-$(uname -r)
+sudo apt install dwarves
+sudo bpftool btf dump file /sys/kernel/btf/vmlinux format c | head
 
 echo "[+] Compiling XDP programs..."
-clang -O2 -target bpf -c cs_ratelimit.c -o cs_ratelimit.o -g -D__TARGET_ARCH_x86
-clang -O2 -target bpf -c sc_amppre.c -o sc_amppre.o -g -D__TARGET_ARCH_x86
+clang -O2 -g -target bpf -D__TARGET_ARCH_x86 -c cs_ratelimit.c -o cs_ratelimit.o
+clang -O2 -g -target bpf -D__TARGET_ARCH_x86 -c sc_amppre.c -o sc_amppre.o
 
 echo "[+] Setting MTU and NIC queues..."
 sudo ip link set ens6 mtu 1500
@@ -19,4 +22,4 @@ echo "[+] Attaching XDP programs..."
 sudo ip link set dev ens6 xdp obj cs_ratelimit.o sec xdp
 sudo ip link set dev ens7 xdp obj sc_amppre.o sec xdp
 
-echo "[✔] Setup completed."
+# /sys/fs/bpf/xdp/globals : check the pinning map 
